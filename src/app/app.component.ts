f23c22c0c89c816ff63520afdfd15f9e1412b3ef
kirtsu04 <lucas.ru74@gmail.com>
2022-09-07 10:03:46 +0200

    change template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Accueil', url: '/folder/Accueil', icon: 'home' },
+    { title: 'Ma salade', url: '/folder/Salade', icon: 'fast-food' },
+    { title: 'Scanner mon QRCode', url: '/folder/QrCode', icon: 'qr-code' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
