827151e3fcb3dde78fc3d1b9668151f8424b5a0c
lucas-ru <lucas.ru74@gmail.com>
2022-09-28 18:28:37 +0200

    change IDE

--- src/app/views/QrCode/qrcode.page.ts
+++ src/app/views/QrCode/qrcode.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {NgxScannerQrcodeModule} from "ngx-scanner-qrcode";
+
 
 
 @Component({
@@ -9,7 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class QrCodePage implements OnInit {
   public output;
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private qrcode: NgxScannerQrcodeModule) { }
+
+  public onError(e: any): void {
+    alert(e);
+  }
+
+  public handle(action: any, fn: string): void {
+    action[fn]().subscribe((res: boolean) =>  console.log(fn + ': ' + res));
+  }
 
   ngOnInit() {}
 }
